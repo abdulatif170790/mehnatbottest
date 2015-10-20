7c1134949d66a3d1e62128c43acefc5fba85aaf3
Abdulatif170790 <abdulatif170790@gmail.com>
2015-10-20 14:49:21 +0500

    sdf

--- TelegramAPI.php
+++ TelegramAPI.php
@@ -3,57 +3,102 @@
 class TelegramAPI
 {
 
-    private $token = "148278479:AAFPMVrxoSPQG3xC1FUFvQL3nNgDOvZ1h-8";
-    private $website;
-    private $update;
-
-    public function getUpdatedArray()
-    {
-        return json_decode($this->update, TRUE);
-    }
+    const TOKEN = "148278479:AAFPMVrxoSPQG3xC1FUFvQL3nNgDOvZ1h-8";
+    const BASE_URL = 'https://api.telegram.org/bot';
+    private $baseURL;
 
     function __construct()
     {
-        $this->website = "https://api.telegram.org/bot" . $this->token;
-        $this->update = file_get_contents("php://input");
+        $this->baseURL = self::BASE_URL . self::TOKEN;
     }
 
-    public function sendMessage($chat_id, $text)
+//    public function sendMessage($chat_id, $text)
+//    {
+//        $url = $this->website . '/sendMessage?' . http_build_query(
+//                array(
+//                    "chat_id" => $chat_id,
+//                    "text" => urlencode($text)
+//                ));
+//        file_get_contents($url);
+//    }
+
+//    public function sendMessageReplyMarkup($chat_id,$text,$disable_web_page_preview = false,$reply_to_message_id = null,$reply_markup = null)
+//    {
+//        $url = $this->website . "/sendMessage?" . http_build_query(
+//                array(
+//                    "chat_id" => $chat_id,
+//                    "text" => $text,
+//                    "disable_web_page_preview" => $disable_web_page_preview,
+//                    "reply_to_message_id" => $reply_to_message_id,
+//                    "reply_markup" => $reply_markup
+//                )
+//            );
+//        file_get_contents($url);
+//    }
+
+    public function sendMessage($chat_id, $text, $parse_mode = null, $disable_web_page_preview = false, $reply_to_message_id = null, $reply_markup = null)
     {
-        $url = $this->website . '/sendMessage?' . http_build_query(
-                array(
-                    "chat_id" => $chat_id,
-                    "text" => urlencode($text)
-                ));
-        file_get_contents($url);
+        $params = compact('chat_id', 'text', 'parse_mode', 'disable_web_page_preview', 'reply_to_message_id', 'reply_markup');
+
+        return $this->sendRequest('sendMessage', $params);
     }
 
-    public function sendMessageReplyMarkup(
-        $chat_id,
-        $text,
-        $disable_web_page_preview = false,
-        $reply_to_message_id = null,
-        $reply_markup = null
-    )
+    public function forwardMessage($chat_id, $from_chat_id, $from_chat_id)
     {
-        $url = $this->website . "/sendMessage?" . http_build_query(
-                array(
-                    "chat_id" => $chat_id,
-                    "text" => $text,
-                    "disable_web_page_preview" => $disable_web_page_preview,
-                    "reply_to_message_id" => $reply_to_message_id,
-                    "reply_markup" => $reply_markup
-                )
-            );
-        file_get_contents($url);
+        $params = compact('chat_id', 'from_chat_id', 'from_chat_id');
+
+        return $this->sendRequest('forwardMessage', $params);
     }
 
-    public function replyKeyboardMarkup(
-        $keyboard,
-        $resize_keyboard = false,
-        $one_time_keyboard = false,
-        $selective = false
-    )
+    public function sendPhoto($chat_id, $photo, $caption = null, $reply_to_message_id = null, $reply_markup = null)
+    {
+        $data = compact('chat_id', 'photo', 'caption', 'reply_to_message_id', 'reply_markup');
+
+        if (((!is_dir($photo)) && (filter_var($photo, FILTER_VALIDATE_URL) === FALSE)))
+            return $this->sendRequest('sendPhoto', $data);
+
+        return $this->uploadFile('sendPhoto', $data);
+    }
+
+    public function sendChatAction($chat_id, $action)
+    {
+        $actions = array(
+            'typing',
+            'upload_photo',
+            'record_video',
+            'upload_video',
+            'record_audio',
+            'upload_audio',
+            'upload_document',
+            'find_location',
+        );
+        if (isset($action) && in_array($action, $actions))
+        {
+            $params = compact('chat_id', 'action');
+            return $this->sendRequest('sendChatAction', $params);
+        }
+
+        throw new Exception('Invalid Action! Accepted value: '.implode(', ', $actions));
+    }
+
+    private function sendRequest($method, $params)
+    {
+        return json_decode(file_get_contents($this->baseURL . $method . '?' . http_build_query($params)), true);
+    }
+
+    public function getWebhookUpdates()
+    {
+        $body = json_decode(file_get_contents('php://input'), true);
+
+        return $body;
+    }
+
+
+
+
+
+
+    public function replyKeyboardMarkup($keyboard, $resize_keyboard = false, $one_time_keyboard = false, $selective = false)
     {
         return json_encode(compact('keyboard', 'resize_keyboard', 'one_time_keyboard', 'selective'));
     }
@@ -61,15 +106,14 @@ class TelegramAPI
     public static function replyKeyboardHide($selective = false)
     {
         $hide_keyboard = true;
-
         return json_encode(compact('hide_keyboard', 'selective'));
     }
 
     public static function forceReply($selective = false)
     {
         $force_reply = true;
-
         return json_encode(compact('force_reply', 'selective'));
     }
+
 }
 
--- index.php
+++ index.php
@@ -1,13 +1,51 @@
 <?php
 
-include_once 'TelegramAPI.php';
 ini_set('error_reporting', E_ALL);
+require 'TelegramAPI.php';
 
 $tg = new TelegramAPI();
-$updateArray = $tg->getUpdatedArray();
-$chatId = $updateArray['message']['chat']['id'];
-$text = $updateArray['message']['text'];
 
+
+$chat_id = null;
+$guessed = false;
+$sendQuestion = false;
+
+
+// Custom keyboard
+$customKeyboard = [
+    ['7', '8', '9'],
+    ['4', '5', '6'],
+    ['1', '2', '3'],
+    ['0']
+];
+$reply_markup = $tg->replyKeyboardMarkup($customKeyboard, true, true);
+
+do {
+    $data = $tg->getWebhookUpdates();
+        if (is_null($chat_id))
+            $chat_id = $data['message']['chat']['id'];
+
+        if (!$sendQuestion) {
+            $tg->sendChatAction($chat_id, 'typing');
+            $tg->sendMessage($chat_id, 'Guess the number', false, null, $reply_markup);
+            $sendQuestion = true;
+        }
+
+        if (($data['message']['text']) == 5) {
+            $tg->sendChatAction($chat_id, 'typing');
+            $tg->sendMessage($chat_id, 'You did it! :)');
+            $tg->sendChatAction($chat_id, 'upload_photo');
+            $tg->sendPhoto($chat_id, 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/718smiley.png/220px-718smiley.png');
+            $guessed = true;
+        } else
+            $tg->sendMessage($chat_id, 'Wrong number :/ try again', false, null, $reply_markup);
+} while (!$guessed);
+$updates = $tg->getWebhookUpdates();
+
+//$updateArray = $tg->getUpdatedArray();
+//$chatId = $updateArray['message']['chat']['id'];
+//$text = $updateArray['message']['text'];
+/*
 switch ($text) {
     case 'Q':
         $keyboard = [
@@ -34,5 +72,5 @@ switch ($text) {
 
 }
 
-
+*/
 ?>
