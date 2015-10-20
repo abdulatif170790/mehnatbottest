a426bb263d999980e0cda2c1ae300f61d2994add
Abdulatif170790 <abdulatif170790@gmail.com>
2015-10-20 18:21:50 +0500

    454

--- index.php
+++ index.php
@@ -2,9 +2,7 @@
 
 ini_set('error_reporting', E_ALL);
 require 'TelegramAPI.php';
-
 $tg = new TelegramAPI();
-
 
 $chat_id = null;
 $guessed = false;
@@ -14,35 +12,31 @@ $data = $tg->getWebhookUpdates();
 $chat_id = $data['message']['chat']['id'];
 $text = $data['message']['text'];
 
-$tg->sendChatAction($chat_id, "javobni kuting...");
-$result = json_decode(file_get_contents('http://mehnat.uz/mehnatbot/mehnatbot.php'));
 
+$tg->sendChatAction($chat_id, "natijani kuting...");
+$result = (array)json_decode(file_get_contents('http://mehnat.uz/mehnatbot/mehnatbot.php'));
+print_r($result);
+echo "<br>------------------------------------------------<br><br>";
 
-if ($text == "/minimalka") {
-    $tg->sendMessage($chat_id, "Hozirgi kunda eng kam oylik ish haqi - ".$result['zarplata']." so'm");
+if (strcmp($text, "/minimalka") === 0) {
+    $natija = "Hozirgi kunda eng kam oylik ish haqi - " . $result['zarplata'] . " so'm";
 }
-
-
 elseif ($tg->startsWith($text, "/maosh")) {
-
     $string = "Maosh hisoblash uchun quyidagi ko'rinishda kiriting (1-21): \nMasalan: \n /maosh 7";
-
-    if ($text == "/maosh") $tg->sendMessage($chat_id, $string);
+    if ($text == "/maosh") $natija = $string;
     else {
-        $r = substr_replace($text, '', 0, 5);
+        $r = substr_replace($text, '', 0, 6);
         $number = (int)$r;
         if ($number >= 0 && $number <= 22) {
-            $r1 = $result['razryad'][$number]['razryad'];
-            $k1 = $result['razryad'][$number]['koef'];
-            $tg->sendMessage($chat_id, "Razryad - ".$r1."\n Koeffitsient tarif - ".$k1."\n Sizning maoshingiz: ".($r1*$k1)." so'm");
-
-        } else $tg->sendMessage($chat_id, "Siz noto'g'ri razryad kiritdingiz:\n\n" . $string);
+            $arr = (array) $result['razryad'][$number];
+            $natija = "Razryad - ".$arr['razryad']."\n Koeffitsient tarif - ".$arr['koef']."\n Sizning maoshingiz: ".($result['zarplata']*$arr['koef'])." so'm";
+        } else $natija = "Siz noto'g'ri razryad kiritdingiz:\n\n";
     }
-
 }
-else{
-    $tg->sendMessage($chat_id, json_encode($result));
+else {
+    $natija = 'else';
 }
+$tg->sendMessage($chat_id, $natija);
 
 
 //    case 'Q':
