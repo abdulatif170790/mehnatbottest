25efc30e218eacfb1d868d4f9aef8d8e4d7864ca
Abdulatif170790 <abdulatif170790@gmail.com>
2015-10-20 12:12:25 +0500

    gd

--- TelegramAPI.php
+++ TelegramAPI.php
@@ -1,7 +1,36 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: root
- * Date: 10/20/15
- * Time: 11:43 AM
- */
+
+class TelegramAPI
+{
+
+    public $token = "148278479:AAFPMVrxoSPQG3xC1FUFvQL3nNgDOvZ1h-8";
+    public $website;
+    public $update;
+
+    public function getUpdatedArray()
+    {
+        return json_decode($this->update, TRUE);
+    }
+
+    public function __construct()
+    {
+        $this->website = "https://api.telegram.org/bot" . $this->token;
+        $this->update = file_get_contents("php://input");
+    }
+
+    public function sendMessage($chat_id, $text)
+    {
+        $url = $GLOBALS['website'] .
+            'sendMessage?' .
+            http_build_query(
+                array(
+                    "chat_id" => $chat_id,
+                    "text" => urlencode($text)
+                )
+            );
+
+        file_get_contents($url);
+    }
+
+}
+
