4ab0583425b8cf3aa983c7174ac318a13ed12609
Abdulatif170790 <abdulatif170790@gmail.com>
2015-10-20 17:07:35 +0500

    sdf

--- index.php
+++ index.php
@@ -15,45 +15,53 @@ $chat_id = $data['message']['chat']['id'];
 $text = $data['message']['text'];
 
 $tg->sendChatAction($chat_id, "javobni kuting...");
+$result = json_decode(file_get_contents('http://localhost/testmehnat/index.php'));
 
-switch ($text) {
-    case 'Q':
-        $keyboard = [
-            ['7', '8', '9'],
-            ['4', '5', '6'],
-            ['1', '2', '3'],
-            ['0']
-        ];
-        $reply_markup = $tg->replyKeyboardMarkup($keyboard, true, true);
-        $tg->sendMessage($chat_id, $text, false, null, $reply_markup);
-        break;
-    case "W":
-        $reply_markup = $tg->replyKeyboardHide();
-        $tg->sendMessage($chat_id, $text, false, null, $reply_markup);
-        break;
-    case "E":
-        $reply_markup = $tg->forceReply();
-        $tg->sendMessage($chat_id, $text, false, null, $reply_markup);
-        break;
-    default:
-        $tg->sendMessage($chat_id, $chat_id.$text);
-        break;
+
+if ($text == "/minimalka") {
+    $tg->sendMessage($chat_id, "Hozirgi kunda eng kam oylik ish haqi - ".$result['zarplata']." so'm");
+}
+elseif ($tg->startsWith($text, "/maosh")) {
+
+    $string = "Maosh hisoblash uchun quyidagi ko'rinishda kiriting (1-21): \nMasalan: \n /maosh 7";
+
+    if ($text == "/maosh") $tg->sendMessage($chat_id, $string);
+    else {
+        $r = substr_replace($text, '', 0, 5);
+        $number = (int)$r;
+        if ($number >= 0 && $number <= 22) {
+            $r1 = $result['razryad'][$number]['razryad'];
+            $k1 = $result['razryad'][$number]['koef'];
+            $tg->sendMessage($chat_id, "Razryad - ".$r1."\n Koeffitsient tarif - ".$k1."\n Sizning maoshingiz: ".($r1*$k1)." so'm");
+
+        } else $tg->sendMessage($chat_id, "Siz noto'g'ri razryad kiritdingiz:\n\n" . $string);
+    }
+
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+//    case 'Q':
+//        $keyboard = [
+//            ['7', '8', '9'],
+//            ['4', '5', '6'],
+//            ['1', '2', '3'],
+//            ['0']
+//        ];
+//        $reply_markup = $tg->replyKeyboardMarkup($keyboard, true, true);
+//        $tg->sendMessage($chat_id, $text, false, null, $reply_markup);
+//        break;
+//    case "W":
+//        $reply_markup = $tg->replyKeyboardHide();
+//        $tg->sendMessage($chat_id, $text, false, null, $reply_markup);
+//        break;
+//    case "E":
+//        $reply_markup = $tg->forceReply();
+//        $tg->sendMessage($chat_id, $text, false, null, $reply_markup);
+//        break;
+//    default:
+//        $tg->sendMessage($chat_id, $chat_id.$text);
+//        break;
+//}
 
 
 //// Custom keyboard
